8ffa9bbe6e9aec75a12d1e08032aa7f5cec6c118
eunowel <wencong.luo@ericsson.com>
2019-08-02 17:53:15 +0800

    modify dockerfile and rename checkHealthController

--- src/main/java/com/ken/demo/controller/CheckHealthController.java
+++ src/main/java/com/ken/demo/controller/CheckHealthController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class CheckHealth {
+public class CheckHealthController {
 
     @RequestMapping(value = "/health")
     public String health()
